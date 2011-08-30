c6d7e1ef2e17389118becf4de165400496d66dc8
Romain <romain@melting-media.net>
2011-08-30 12:51:19 +0200

    First draft for French translation

---
+++ core/components/cliche/lexicon/fr/default.inc.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Default French Lexicon Entries for Cliche
+ *
+ * @package cliche
+ * @subpackage lexicon
+ */
+/* TOP MENU */
+
+$_lang['cliche'] = 'Cliche';
+$_lang['cliche.menu_desc'] = 'Une galerie d\'images dynamique pour MODx Revolution.';
+
+$_lang['cliche.no_albums'] = '[Cliche] Aucun album créé pour le moment<br/>';
+$_lang['cliche.album_not_found'] = '[Cliche] Album non trouvé<br/>';
+$_lang['cliche.item_not_found'] = '[Cliche] Entrée non trouvée<br/>';
+
+/* file uploader */
+$_lang['cliche.zip_error_unpack'] = 'Erreur lors de la décompression';
+$_lang['cliche.xpdozip_not_found'] = 'xPDOZip n\'a pu être chargé';
+
+/* Processors error messages - to move into mgr lexicon */
+$_lang['cliche.error_album_not_created'] = 'L\'album n\'a pu être créé';
+$_lang['cliche.error_album_create_name_already_taken'] = 'Un album portant ce nom existe déjà. Veuillez choisir un autre nom';
+$_lang['cliche.error_album_delete_cancelled'] = 'Erreur - L\'album n\'a pu être supprimé. Opération interrompue, veuillez contacter votre administrateur';
+$_lang['cliche.error_album_delete_no_id'] = 'Erreur - ID d\'album eronné ou non indiqué';
+$_lang['cliche.error_delete_item_aborted'] = 'Erreur - L\'entrée n\'a pu être supprimée - Opération interrompue, veuillez contacter votre administrateur';
+$_lang['cliche.error_delete_item_no_id'] = 'Erreur - ID d\'entrée eronné ou non indiqué';
+
+
+/* Processors success messages - to move into mgr lexicon */
+$_lang['cliche.album_created_succesfully'] = 'Album créé avec succès';
+$_lang['cliche.album_udpated_succesfully'] = 'Album mis à jour avec succès';
+$_lang['cliche.album_deleted_successfully'] = 'Album et entrées supprimés avec succès';
+$_lang['cliche.item_deleted_succesfully'] = 'Entrée supprimée avec succès';
+$_lang['cliche.item_set_as_cover_succesfully'] = 'Image définie en tant que couverture avec succès';
