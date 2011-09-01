7a0f5ec66445367c85668aae2d8de951c7511c23
lossendae <lossendae@gmail.com>
2011-09-02 00:14:41 +0200

    Was it CLRF ?

--- core/components/cliche/elements/snippets/snippet.clicheitem.php
+++ core/components/cliche/elements/snippets/snippet.clicheitem.php
@@ -1,1 +1,20 @@
-<?php/** * Snippet ClicheItem * * @package cliche *//** * Cliche * * A Gallery manager components for MODx Revolution * * @author Stephane Boulard <lossendae@gmail.com> * @package cliche */$Cliche = $modx->getService('cliche','Cliche',$modx->getOption('cliche.core_path',null,$modx->getOption('core_path').'components/cliche/').'model/cliche/',$scriptProperties);if (!($Cliche instanceof Cliche)) return 'Cliche could not be loaded';$controller = $Cliche->loadController('Item');$output = $controller->run($scriptProperties);return $output;
+<?php
+/**
+ * Snippet ClicheItem
+ *
+ * @package cliche
+ */
+/**
+ * Cliche
+ *
+ * A Gallery manager components for MODx Revolution
+ *
+ * @author Stephane Boulard <lossendae@gmail.com>
+ * @package cliche
+ */
+$Cliche = $modx->getService('cliche','Cliche',$modx->getOption('cliche.core_path',null,$modx->getOption('core_path').'components/cliche/').'model/cliche/',$scriptProperties);
+if (!($Cliche instanceof Cliche)) return 'Cliche could not be loaded';
+
+$controller = $Cliche->loadController('Item');
+$output = $controller->run($scriptProperties);
+return $output;
