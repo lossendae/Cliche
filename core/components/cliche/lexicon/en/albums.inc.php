bbb795a89bcfbf49c277e00f4d07e03f3205a36c
lossendae <lossendae@gmail.com>
2011-11-26 11:07:06 +0100

    Updated lexicon strings

---
+++ core/components/cliche/lexicon/en/albums.inc.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Default Manager English Lexicon Entries for Cliche
+ *
+ * @package cliche
+ * @subpackage lexicon
+ */
+ 
+/* CARD - ALBUMS LIST */
+
+//buttons
+$_lang['cliche.add_album_btn'] = 'Create a new album';
+
+//Create or update album window
+$_lang['cliche.album_name_label'] = 'Album name (required)';
+$_lang['cliche.album_desc_label'] = 'Quick description';
+$_lang['cliche.create_album_title'] = 'Your album';
+$_lang['cliche.save_album_btn'] = 'Save';
+
+//Error messages from the backend
+
+
+//view
+$_lang['cliche.album-list.empty_msg'] = '<h4>There are no albums created yet</h4><p>Create your first album using the button above, then click the newly created item to view its content.<br/>Use the breadcrumbs to navigate back and forth in the albums (The current green element is not clickable)</p>';
+$_lang['cliche.album-empty-col-msg'] = 'Select an item from the main column to view its description';
+$_lang['cliche.list.empty_col_msg'] = '<div class="empty-msg">Select an album from the main column to view its description</div>';
+$_lang['cliche.main_total_pics'] = '{total} Pictures';
+
+//msg
+$_lang['cliche.breadcrumb_album_list_desc'] = 'Album List.<br/>Select an album to view its contents.';
+
+
+/* CARD - ITEMS LIST */
+
+//View
+$_lang['cliche.items_empty_msg'] = '<h4>There are no pictures in this album</h4><p>Add pictures by clicking the button above</p>';
+$_lang['cliche.items_empty_col_msg'] = '<div class="empty-msg">Select a picture from the main column to see its description</div>';
+$_lang['cliche.album_cover_alt_msg'] = '{name} preview';
+$_lang['cliche.view_total_pics'] = 'Total Pictures';
+//buttons
+$_lang['cliche.add_images'] = 'Add Photos';
+$_lang['cliche.update_album'] = 'Update album';
+$_lang['cliche.delete_album'] = 'Delete album';
+
+//msg
+$_lang['cliche.delete_album_msg'] = 'All pitctures in this album will also be deleted. This operation is irreversible.';
+$_lang['cliche.breadcrumbs_album_msg'] = '<div class="album_desc">
+	<tpl if="cover_id == 0">
+		<span class="no-preview">No preview</span>
+	</tpl>
+	<tpl if="cover_id">
+		<img src="{thumbnail}" title="Image Cover for the album {name}" alt="Image Cover for the album {name}" />
+	</tpl>
+	<div class="album_name">Album : {name}
+		<span>{total} Photos - Created on {createdon} by <b>{createdby}</b></span>
+	</div>
+	<button class="inline-button green" onclick="Ext.getCmp(\'cliche-album-default\').onaddPhoto(); return false;"/><span class="icon-add-white">&nbsp;</span>Add photo</button>
+</div>';
+
+/* CARD - SINGLE ITEM */
+
+//buttons
+$_lang['cliche.edit_item'] = 'Edit image';
+$_lang['cliche.delete_image'] = 'Delete image';
+$_lang['cliche.set_as_album_cover'] = 'Set as Album cover';
+
+//msg
+$_lang['cliche.delete_image_title'] = 'Remove picture';
+$_lang['cliche.delete_image_msg'] = 'Are you sure you want to delete this image ? This operation is irreversible.';
+$_lang['cliche.breadcrumbs_item_msg'] = 'Viewing picture : ';
+
+/* CARD - UPLOAD ITEMS */
+
+//view
+$_lang['cliche.upload_desc'] = '<h4>Select files from your computer</h4><p>You can select several files at a time by holding the shift key.</p>';
+$_lang['cliche.upload_ready_msg'] = 'Files ready to be uploaded :';
+$_lang['cliche.upload_cancel_msg'] = 'Cancel';
+$_lang['cliche.upload_success_msg'] = 'Image uploaded sucessfully'; 
+$_lang['cliche.upload_fail_msg'] = 'The image could not be saved';
+
+//buttons
+$_lang['cliche.browse'] = 'Browse...';
+$_lang['cliche.start_upload'] = 'Start Upload';
+
+//msg
+$_lang['cliche.breadcrumbs_upload_pictures_msg'] = 'Upload pictures'; 
+$_lang['cliche.upload_in_progress'] = 'Upload in progress...'; 
+$_lang['cliche.upload_items_for'] = 'Upload Items for the album '; 
+$_lang['cliche.upload_successful'] = 'Files uploaded successfully'; 
+
+//NOT USED YET
+$_lang['cliche.global_progressbar_loading_item'] = 'Loading item'; 
+$_lang['cliche.item_progressbar'] = '% completed...'; 
+
+
+/* MGR - COMMONS */
+//tabs
+$_lang['cliche.main_tab'] = 'Manage your albums';
+//Breadcrumbs
+$_lang['cliche.breadcrumb_root'] = 'Album list';
+//buttons
+$_lang['cliche.close_btn'] = 'Cancel';
+//misc
+$_lang['cliche.actions'] = 'Actions';
+$_lang['cliche.sort_by'] = 'Sort by';
+$_lang['cliche.created_by'] = 'Created by';
+$_lang['cliche.created_on'] = 'Created on';
+// $_lang['cliche.sort_by_createdon'] = 'Creation date';
+// $_lang['cliche.sort_by_total_pics'] = 'Number of pictures';
+// $_lang['cliche.sort_by_alphabetically'] = 'Alphabetically';
+$_lang['cliche.desc_title'] = 'Description';
+$_lang['cliche.informations_title'] = 'Informations';
+$_lang['cliche.no_desc'] = '<em>No description</em>';
+$_lang['cliche.no_preview'] = 'No preview';
+$_lang['cliche.loading'] = '<div class="centered empty-msg">Loading...</div>';
--- core/components/cliche/lexicon/en/default.inc.php
+++ core/components/cliche/lexicon/en/default.inc.php
@@ -7,8 +7,9 @@
  */
 /* TOP MENU */
 
-$_lang['cliche'] = 'Cliche';
-$_lang['cliche.menu_desc'] = 'A dynamic Image Gallery for MODx Revolution.';
+$_lang['cliche'] = 'Cliche, Albums manager';
+$_lang['cliche.menu'] = 'Cliche';
+$_lang['cliche.menu_desc'] = 'A media manager for MODx Revolution.';
 
 $_lang['cliche.no_albums'] = '[Cliche] There are no albums created yet<br/>';
 $_lang['cliche.album_not_found'] = '[Cliche] Album not found<br/>';
@@ -19,17 +20,16 @@ $_lang['cliche.zip_error_unpack'] = 'Error while unpacking';
 $_lang['cliche.xpdozip_not_found'] = 'xPDOZip could not be loaded';
 
 /* Processors error messages - to move into mgr lexicon */
-$_lang['cliche.error_album_not_created'] = 'Album could not be created';
+$_lang['cliche.error_album_not_created'] = 'Album could not be not created';
 $_lang['cliche.error_album_create_name_already_taken'] = 'The album name already exists. Choose another name';
-$_lang['cliche.error_album_delete_cancelled'] = 'Error - The album could not be removed - Operation aborted, please contact the webmaster';
+$_lang['cliche.error_album_delete_cancelled'] = 'Error - The album could not be removed. Operation aborted, please contact the webmaster';
 $_lang['cliche.error_album_delete_no_id'] = 'Error - The album id was either false or not supplied';
 $_lang['cliche.error_delete_item_aborted'] = 'Error - The item could not be removed - Operation aborted, please contact the webmaster';
 $_lang['cliche.error_delete_item_no_id'] = 'Error - The item id was either false or not supplied';
 
-
 /* Processors success messages - to move into mgr lexicon */
-$_lang['cliche.album_created_succesfully'] = 'Album successfully created';
-$_lang['cliche.album_udpated_succesfully'] = 'Album successfully updated';
-$_lang['cliche.album_deleted_successfully'] = 'Album and items successfully deleted';
-$_lang['cliche.item_deleted_succesfully'] = 'Item successfully removed';
-$_lang['cliche.item_set_as_cover_succesfully'] = 'Image successfully set as album cover';
+$_lang['cliche.album_created_succesfully'] = 'Album created successfully';
+$_lang['cliche.album_udpated_succesfully'] = 'Album created successfully';
+$_lang['cliche.album_deleted_successfully'] = 'Album and items deleted successfully';
+$_lang['cliche.item_deleted_succesfully'] = 'Item removed successfully';
+$_lang['cliche.item_set_as_cover_succesfully'] = 'Image set as album cover successfully';
