05ad83ea204432fbbf34384dea2d3a029accf7ea
lossendae <lossendae@gmail.com>
2012-01-10 16:13:58 +0100

    Updated default plugin

--- assets/components/cliche/plugins/default/default.class.php
+++ assets/components/cliche/plugins/default/default.class.php
@@ -13,20 +13,34 @@ class DefaultPlugin extends ClichePlugin {
      */
     public function notify( $event, $args = null ){
 		$view = $this->obj->getProperty('view');
-		$browse = $this->obj->getProperty('browse');
-		/* We're not in browse mode - Use an alternative chunk for each item when not in image view */
-		if(!$browse){
-			$this->obj->setProperties(array(
-				'albumItemTpl' => 'albumcoverzoom',
-				'itemTpl' => 'itemzoom',
-			));
+		$browse = $this->obj->getProperty('browse');	
+		switch($event){
+			case 'load':
+				/* We're not in browse mode - Use an alternative chunk for each item when not in image view */
+				if(!$browse){
+					$this->obj->setProperties(array(
+						'albumItemTpl' => 'albumcoverzoom',
+						'itemTpl' => 'itemzoom',
+					));
+				}	
+				$this->useFancyBox = $this->obj->getProperty('useFancyBox', true);						
+				break;
+			case 'render':
+				$this->obj->loadCSS('style');
+				/* Load fancybox only if we are viewing a single image and/or we're not in browse mode */
+				if($this->useFancyBox && $view == 'image' || $this->useFancyBox && !$browse){
+					$this->loadJquery = $this->obj->getProperty('loadJquery', true);
+					if($this->loadJquery){
+						$this->modx->regClientStartupScript('http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js');
+					}			
+					$this->modx->regClientStartupScript($this->obj->config['plugins_url'] . 'default/fancybox/jquery.fancybox-1.3.4.pack.js');		
+					
+					$script = $this->obj->getProperty('js', 'script');
+					$this->modx->regClientHTMLBlock('<script type="text/javascript">'. $this->obj->getChunk($script) .'</script>');
+				}
+				break;
+			default:
+				break;
 		}		
-		/* Load fancybox only if we are viewing a single image or we're not in browse mode */
-		if($view == 'image' || !$browse){
-			$this->modx->regClientStartupScript('http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js');
-			$this->modx->regClientStartupScript($this->obj->config['plugins_url'] . 'default/fancybox/jquery.fancybox-1.3.4.pack.js');
-			
-			$this->modx->regClientHTMLBlock('<script type="text/javascript">$("a.zoom").fancybox();</script>');
-		}
 	}
 }
