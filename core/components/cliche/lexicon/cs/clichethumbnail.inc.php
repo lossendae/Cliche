c5b047cebe99a02bbeba55f178431d46a071157f
Jan Tezner <jan.tezner@gmail.com>
2012-01-20 10:15:36 +0000

    Czech translation of complete Cliche package lexicons

---
+++ core/components/cliche/lexicon/cs/clichethumbnail.inc.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Czech Lexicon Entries for ClicheThumbnail
+ *
+ * @package cliche
+ * @subpackage lexicon
+*
+ * @author modxcms.cz
+ * @updated 2012-01-20
+ */
+
+// $_lang['clichethumbnail.main_default_text'] = '<h4>Click the button above to select an image</h4>The preview of the selected image will replace this text';
+$_lang['clichethumbnail.main_default_text'] = '<h4>Výběr obrázku provedete kliknutím na tlačítko výše</h4>Tento text pak bude nahrazen náhledem vybraného obrázku';
+// $_lang['clichethumbnail.main_your_preview'] = 'Thumbnail preview';
+$_lang['clichethumbnail.main_your_preview'] = 'Miniatura náhledu';
+
+// $_lang['clichethumbnail.window_title'] = 'Thumbnail Manager';
+$_lang['clichethumbnail.window_title'] = 'Správce miniatur';
+
+// $_lang['clichethumbnail.breadcrumb_album'] = 'Album browser';
+$_lang['clichethumbnail.breadcrumb_album'] = 'Průzkumník alb';
+// $_lang['clichethumbnail.breadcrumb_crop'] = 'Crop Image';
+$_lang['clichethumbnail.breadcrumb_crop'] = 'Ořez obrázku';
+// $_lang['clichethumbnail.breadcrumb_crop_desc'] = '<h3>Cropping Tool</h3><p>Use the left image (original size) to adjust the selection to the right thumbnail (thumbnail preview)</p>';
+$_lang['clichethumbnail.breadcrumb_crop_desc'] = '<h3>Ořezávací nástroj</h3><p>Použijte levého obrázku (původní velikost) pro nastavení výřezu pro pravou miniaturu (náhled miniatury)</p>';
+// $_lang['clichethumbnail.breadcrumb_root'] = 'Album list';
+$_lang['clichethumbnail.breadcrumb_root'] = 'Seznam alb';
+// $_lang['clichethumbnail.breadcrumb_root_desc'] = '<h3>Thumbnail Preview</h3><p>Select an image from the dedicated album</p>';
+$_lang['clichethumbnail.breadcrumb_root_desc'] = '<h3>Náhled miniatury</h3><p>Vyberte obrázek z daného alba</p>';
+// $_lang['clichethumbnail.breadcrumb_root_desc_with_thumb'] = '<h3>Thumbnail Preview</h3><p>You can select a specific part of the image by clicking on the button "Crop Image"</p>';
+$_lang['clichethumbnail.breadcrumb_root_desc_with_thumb'] = '<h3>Náhled miniatury</h3><p>Pokud chcete zvolit pouze určitou část obrázku klikněte na tlačítko "Ořez obrázku"</p>';
+// $_lang['clichethumbnail.breadcrumb_upload'] = 'Add images';
+$_lang['clichethumbnail.breadcrumb_upload'] = 'Přidat obrázky';
+
+// $_lang['clichethumbnail.btn_add_photo'] = 'Add Images';
+$_lang['clichethumbnail.btn_add_photo'] = 'Přidat obrázky';
+// $_lang['clichethumbnail.btn_back_to_album'] = 'Back to album';
+$_lang['clichethumbnail.btn_back_to_album'] = 'Zpět do alba';
+// $_lang['clichethumbnail.btn_back_to_main'] = 'Back to main panel';
+$_lang['clichethumbnail.btn_back_to_main'] = 'Zpět na hlavní panel';
+// $_lang['clichethumbnail.btn_browse'] = 'Browse...';
+$_lang['clichethumbnail.btn_browse'] = 'Vybrat obrázky...';
+// $_lang['clichethumbnail.btn_browse_album'] = 'Browse album';
+$_lang['clichethumbnail.btn_browse_album'] = 'Prohlížet album';
+// $_lang['clichethumbnail.btn_crop_thumbnail'] = 'Crop Image';
+$_lang['clichethumbnail.btn_crop_thumbnail'] = 'Ořez obrázku';
+// $_lang['clichethumbnail.btn_crop_validate'] = 'Commit Changes';
+$_lang['clichethumbnail.btn_crop_validate'] = 'Potvrdit změny';
+// $_lang['clichethumbnail.btn_replace_thumbnail'] = 'Select another image';
+$_lang['clichethumbnail.btn_replace_thumbnail'] = 'Vyberte jiný obrázek';
+// $_lang['clichethumbnail.btn_remove_thumbnail'] = 'Reset';
+$_lang['clichethumbnail.btn_remove_thumbnail'] = 'Reset';
+// $_lang['clichethumbnail.btn_select_image'] = 'Select this image
+$_lang['clichethumbnail.btn_select_image'] = 'Vybrat tento obrázek';
+// $_lang['clichethumbnail.btn_start_upload'] = 'Start Upload';
+$_lang['clichethumbnail.btn_start_upload'] = 'Začít nahrávání';
+// $_lang['clichethumbnail.main_empty_msg'] = '<h4>There is no thumbnail set yet for this document</p>';
+$_lang['clichethumbnail.main_empty_msg'] = '<h4>Pro tento dokument ještě nebyla vybrána miniatura</p>';
+
+// $_lang['clichethumbnail.album_empty_msg'] = '<h4>There are no images in this album</h4><p>Upload images by clicking on "Add images"</p>';
+$_lang['clichethumbnail.album_empty_msg'] = '<h4>Album neobsahuje žádné obrázky</h4><p>Obrázky přidáte kliknutím na zelené tlačítko "Přidat obrázky"';
+// $_lang['clichethumbnail.album_loading'] = '<div class="centered empty-msg">Loading...</div>';
+$_lang['clichethumbnail.album_loading'] = '<div class="centered empty-msg">Probíhá načítání...</div>';
+
+// $_lang['clichethumbnail.image_cover_alt_msg'] = '{name} preview';
+$_lang['clichethumbnail.image_cover_alt_msg'] = '{name} náhled';
+// $_lang['clichethumbnail.image_created_by'] = 'Created by';
+$_lang['clichethumbnail.image_created_by'] = 'Autor';
+// $_lang['clichethumbnail.image_created_on'] = 'Created on';
+$_lang['clichethumbnail.image_created_on'] = 'Vytvořeno';
+// $_lang['clichethumbnail.image_desc_title'] = 'Description';
+$_lang['clichethumbnail.image_desc_title'] = 'Popis';
+// $_lang['clichethumbnail.image_informations_title'] = 'Informations';
+$_lang['clichethumbnail.image_informations_title'] = 'Informace';
+// $_lang['cliche.image_unselected_msg'] = '<div class="empty-msg">Select an image from the main column to see its description</div>';
+$_lang['cliche.image_unselected_msg'] = '<div class="empty-msg">Pro zobrazení informací o obrázku klikněte na nějaký obrázek ze seznamu.</div>';
+// $_lang['clichethumbnail.image_no_desc'] = '<em>No description</em>';
+$_lang['clichethumbnail.image_no_desc'] = '<em>Bez popisu</em>';
+// $_lang['clichethumbnail.image_no_preview'] = 'No preview';
+$_lang['clichethumbnail.image_no_preview'] = 'Bez náhledu';
+
+// $_lang['clichethumbnail.upload_cancel'] = 'Cancel';
+$_lang['clichethumbnail.upload_cancel'] = 'Storno';
+// $_lang['clichethumbnail.upload_desc'] = '<h4>Select files from your computer</h4><p>You can select several files at a time by holding the shift key.</p>';
+$_lang['clichethumbnail.upload_desc'] = '<h4>Vyberte obrázky, které chcete nahrát na server kliknutím na tlačítko "Vybrat obrázky..."</h4><p>Pokud chcete vybrat zároveň více obrázku můžete použít přidržení klávesy CTRL (pro výběr obrázků) nebo SHIFT (pro sadu obrázků).</p>';
+// $_lang['clichethumbnail.upload_in_progress'] = 'Upload in progress...';
+$_lang['clichethumbnail.upload_in_progress'] = 'Probíhá nahrávání...';
+// $_lang['clichethumbnail.upload_ready_msg'] = 'Files ready to be uploaded :';
+$_lang['clichethumbnail.upload_ready_msg'] = 'Obrázky připravené k nahrání :';
+
+// $_lang['clichethumbnail.cropper_empty_msg'] = '<h4>Loading...</p>';
+$_lang['clichethumbnail.cropper_empty_msg'] = '<h4>Momentíček...</p>';
---
+++ core/components/cliche/lexicon/cs/default.inc.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Czech Lexicon Entries for Cliche
+ *
+ * @package cliche
+ * @subpackage lexicon
+ *
+ * @author modxcms.cz
+ * @updated 2012-01-20
+ */
+/* TOP MENU */
+
+// $_lang['cliche'] = 'Cliche, Albums manager';
+$_lang['cliche'] = 'Cliche, správce alb';
+// $_lang['cliche.menu'] = 'Cliche';
+$_lang['cliche.menu'] = 'Cliche';
+// $_lang['cliche.menu_desc'] = 'A media manager for MODx Revolution.';
+$_lang['cliche.menu_desc'] = 'Správce médií pro MODX Revolution.';
+
+// $_lang['setting_cliche.album_mgr_panels'] = 'Album Manager Panels';
+$_lang['setting_cliche.album_mgr_panels'] = 'Panely správce alb';
+// $_lang['setting_cliche.album_mgr_panels_desc'] = 'List of panels to load on the manager panel';
+$_lang['setting_cliche.album_mgr_panels_desc'] = 'Seznam panelů, které se mají nahrát na panel správce';
+
+/* file uploader */
+// $_lang['cliche.album_id_error'] = '[Cliche] Album id not specified';
+$_lang['cliche.album_id_error'] = '[Cliche] Nebylo specifikováno ID alba';
+// $_lang['cliche.already_exist_error'] = '[Cliche] File already exist';
+$_lang['cliche.already_exist_error'] = '[Cliche] Soubor již existuje';
+// $_lang['cliche.create_temp_dir_error'] = '[Cliche] Could not create temp directory';
+$_lang['cliche.create_temp_dir_error'] = '[Cliche] Nepodařilo se vytvořit adresář temp';
+// $_lang['cliche.db_save_item_error'] = '[Cliche] Could not save item "[[+filename]]" in database';
+$_lang['cliche.db_save_item_error'] = '[Cliche] Nelze uložit položku "[[+filename]]" do databáze';
+// $_lang['cliche.empty_file_error'] = '[Cliche] File appears to be empty : ';
+$_lang['cliche.empty_file_error'] = '[Cliche] Soubor nemá žádný obsah : ';
+// $_lang['cliche.file_too_large_error'] = '[Cliche] File is too large';
+$_lang['cliche.file_too_large_error'] = '[Cliche] Soubor je příliš velký';
+// $_lang['cliche.increase_post_max_size'] = '[Cliche] Increase post_max_size and upload_max_filesize to ';
+$_lang['cliche.increase_post_max_size'] = '[Cliche] Zvyšte nastavení post_max_size a upload_max_filesize na hodnotu ';
+// $_lang['cliche.invalid_extensions_error'] = '[Cliche] Invalid file extension, only the following extensions are accepted : ';
+$_lang['cliche.invalid_extensions_error'] = '[Cliche] Neplatná přípona souboru, plátné jsou pouze přípony : ';
+// $_lang['cliche.misc_error'] = '[Cliche] File could not be uploaded';
+$_lang['cliche.misc_error'] = '[Cliche] Soubor nelze nahrát';
+// $_lang['cliche.no_file_error'] = '[Cliche] No file were uploaded';
+$_lang['cliche.no_file_error'] = '[Cliche] Nebyl nahrán žádný soubor';
+// $_lang['cliche.target_dir_error'] = '[Cliche] Could not create the target directory in : ';
+$_lang['cliche.target_dir_error'] = '[Cliche] Nelze vytvořit cílový adresář v umístění : ';
+// $_lang['cliche.target_dir_write_error'] = '[Cliche] Could not write to directory: ';
+$_lang['cliche.target_dir_write_error'] = '[Cliche] Nelze zapisovat do adresáře: ';
+// $_lang['cliche.uploadxhr_error'] = '[Cliche] Could not load helper class UploadFileXhr';
+$_lang['cliche.uploadxhr_error'] = '[Cliche] Nelze načíst pomocnou třídu UploadFileXhr';
+// $_lang['cliche.uploadfileform_error'] = '[Cliche] Could not load helper class UploadFileForm from: ';
+$_lang['cliche.uploadfileform_error'] = '[Cliche] Nelze načíst pomocnou třídu UploadFileForm z umístění: ';
+// $_lang['cliche.image_upload_success_msg'] = 'Image uploaded sucessfully';
+$_lang['cliche.image_upload_success_msg'] = 'Nahrávání obrázků dokončeno';
+// $_lang['cliche.upload_zip_success'] = 'Zip file uploaded successfully - [[+count]] pictures created';
+$_lang['cliche.upload_zip_success'] = 'Nahrání Zip souboru dokončeno - vytvořeno [[+count]] obrázků';
+// $_lang['cliche.xpdozip_not_found'] = '[Cliche] xPDOZip could not be loaded';
+$_lang['cliche.xpdozip_not_found'] = '[Cliche] Nelze načíst xPDOZip';
+// $_lang['cliche.zip_error_unpack'] = '[Cliche] Error while unpacking';
+$_lang['cliche.zip_error_unpack'] = '[Cliche] Chyba při rozbalování';
+
+/* Processors */
+// $_lang['cliche.album_created_succesfully'] = 'Album created successfully';
+$_lang['cliche.album_created_succesfully'] = 'Album vytvořeno';
+// $_lang['cliche.album_deleted_successfully'] = 'Album and items deleted successfully';
+$_lang['cliche.album_deleted_successfully'] = 'Album a jeho obrázky byly odstraněny';
+// $_lang['cliche.album_not_found'] = '[Cliche] Album not found<br/>';
+$_lang['cliche.album_not_found'] = '[Cliche] Album nenalezeno<br/>';
+// $_lang['cliche.album_not_specified'] = '[Cliche] Album not specified<br/>';
+$_lang['cliche.album_not_specified'] = '[Cliche] Album nebylo specifikováno<br/>';
+// $_lang['cliche.album_udpated_succesfully'] = 'Album created successfully';
+$_lang['cliche.album_udpated_succesfully'] = 'Album vytvořeno';
+// $_lang['cliche.error_album_create_name_already_taken'] = 'The album name already exists. Choose another name';
+$_lang['cliche.error_album_create_name_already_taken'] = 'Zadaný název alba již existuje. Zadejte jiný název';
+// $_lang['cliche.error_album_delete_cancelled'] = 'Error - The album could not be removed. Operation aborted, please contact the webmaster';
+$_lang['cliche.error_album_delete_cancelled'] = 'Chyba - Album nelze odstranit. Operace byla přerušena, kontaktujte prosím správce webu';
+// $_lang['cliche.error_album_delete_no_id'] = 'Error - The album id was either false or not supplied';
+$_lang['cliche.error_album_delete_no_id'] = 'Chyba - ID alba bylo buď false nebo nebylo uvedeno';
+// $_lang['cliche.error_delete_item_aborted'] = 'Error - The item could not be removed - Operation aborted, please contact the webmaster';
+$_lang['cliche.error_delete_item_aborted'] = 'Chyba - Položku nelze odstranit - Operace byla přerušena, kontaktujte prosím správce webu';
+// $_lang['cliche.error_delete_item_no_id'] = 'Error - The item id was either false or not supplied';
+$_lang['cliche.error_delete_item_no_id'] = 'Chyba - ID položky bylo buď false nebo nebylo uvedeno';
+// $_lang['cliche.error_album_not_created'] = 'Album could not be not created';
+$_lang['cliche.error_album_not_created'] = 'Album nelze vytvořit';
+// $_lang['cliche.item_deleted_succesfully'] = 'Item removed successfully';
+$_lang['cliche.item_deleted_succesfully'] = 'Položka ostraněna';
+// $_lang['cliche.item_not_found'] = '[Cliche] Item not found<br/>';
+$_lang['cliche.item_not_found'] = '[Cliche] Položka nenalezena<br/>';
+// $_lang['cliche.item_not_specified'] = '[Cliche] No item specified<br/>';
+$_lang['cliche.item_not_specified'] = '[Cliche] Nebyla specifikována položka<br/>';
+// $_lang['cliche.item_set_as_cover_succesfully'] = 'Image set as album cover successfully';
+$_lang['cliche.item_set_as_cover_succesfully'] = 'Obrázek nastaven jako titulní obrázek alba';
+// $_lang['cliche.no_albums'] = '[Cliche] There are no albums created yet<br/>';
+$_lang['cliche.no_albums'] = '[Cliche] Doposud nebylo vytvořené žádné album<br/>';
