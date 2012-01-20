e616d7304a210f1f3ad962264cf53ec18e3f9d4f
Romain <romain@meltingmedia.net>
2012-01-20 01:34:00 +0100

    French translation

---
+++ core/components/cliche/lexicon/fr/clichethumbnail.inc.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Default French Lexicon Entries for ClicheThumbnail
+ *
+ * @package cliche
+ * @subpackage lexicon
+ */
+
+$_lang['clichethumbnail.main_default_text'] = '<h4>Cliquez le bouton ci-dessous pour sélectionner une image</h4>L\'aperçu de l\'image selectionnée remplacera ce texte';
+$_lang['clichethumbnail.main_your_preview'] = 'Aperçu de miniature';
+
+$_lang['clichethumbnail.window_title'] = 'Gestionnaire de miniature';
+
+$_lang['clichethumbnail.breadcrumb_album'] = 'Choix de d\'album';
+$_lang['clichethumbnail.breadcrumb_crop'] = 'Rogner l\'image';
+$_lang['clichethumbnail.breadcrumb_crop_desc'] = '<h3>Outil de rognage</h3><p>Utilisez l\'image de gauche (taille originale) pour ajuster la miniature (aperçu à gauche)</p>';
+$_lang['clichethumbnail.breadcrumb_root'] = 'Liste d\'album';
+$_lang['clichethumbnail.breadcrumb_root_desc'] = '<h3>Aperçu de miniature</h3><p>Sélectionnez une image depuis l\'album souhaité</p>';
+$_lang['clichethumbnail.breadcrumb_root_desc_with_thumb'] = '<h3>Aperçu de miniature</h3><p>Vous pouvez choisir une portion de l\'image en cliquant sur le bouton « Rogner l\'image »</p>';
+$_lang['clichethumbnail.breadcrumb_upload'] = 'Ajouter des images';
+
+$_lang['clichethumbnail.btn_add_photo'] = 'Ajouter des images';
+$_lang['clichethumbnail.btn_back_to_album'] = 'Retour à l\'album';
+$_lang['clichethumbnail.btn_back_to_main'] = 'Retour à l\'écran principal';
+$_lang['clichethumbnail.btn_browse'] = 'Parcourir…';
+$_lang['clichethumbnail.btn_browse_album'] = 'Parcourir l\'album';
+$_lang['clichethumbnail.btn_crop_thumbnail'] = 'Rogner l\'image';
+$_lang['clichethumbnail.btn_crop_validate'] = 'Appliquer les changements';
+$_lang['clichethumbnail.btn_replace_thumbnail'] = 'Sélectionner une autre image';
+$_lang['clichethumbnail.btn_remove_thumbnail'] = 'Annuler';
+$_lang['clichethumbnail.btn_select_image'] = 'Sélectionner cette image';
+$_lang['clichethumbnail.btn_start_upload'] = 'Démarrer l\'upload';
+
+$_lang['clichethumbnail.main_empty_msg'] = '<h4>Il n\'y a pas d\'aperçu de défini pour ce document</p>';
+
+$_lang['clichethumbnail.album_empty_msg'] = '<h4>Il n\'y a pas d\'image dans cet album</h4><p>Ajoutez des images en cliquant sur « Ajouter des images »</p>';
+$_lang['clichethumbnail.album_loading'] = '<div class="centered empty-msg">Chargement…</div>';
+
+$_lang['clichethumbnail.image_cover_alt_msg'] = '{name} aperçu';
+$_lang['clichethumbnail.image_created_by'] = 'Créé par';
+$_lang['clichethumbnail.image_created_on'] = 'Créé le';
+$_lang['clichethumbnail.image_desc_title'] = 'Description';
+$_lang['clichethumbnail.image_informations_title'] = 'Informations';
+$_lang['cliche.image_unselected_msg'] = '<div class="empty-msg">Sélectionnez une image depuis la colonne principale pour voir sa description</div>';
+$_lang['clichethumbnail.image_no_desc'] = '<em>Aucune description</em>';
+$_lang['clichethumbnail.image_no_preview'] = 'Aucun aperçu';
+
+$_lang['clichethumbnail.upload_cancel'] = 'Annuler';
+$_lang['clichethumbnail.upload_desc'] = '<h4>Sélectionnez des fichiers depuis votre ordinateur</h4><p>Vous pouvez sélectionner plusieurs fichiers en même temps en maintenant la touche shift.</p>';
+$_lang['clichethumbnail.upload_in_progress'] = 'Upload en cours…';
+$_lang['clichethumbnail.upload_ready_msg'] = 'Fichiers prêts à être uploadés :';
+
+$_lang['clichethumbnail.cropper_empty_msg'] = '<h4>Chargement…</p>';
