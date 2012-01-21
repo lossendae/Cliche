dc2fccc9f0cdd64b25fdf2362c6cee494eff4f6f
lossendae <lossendae@gmail.com>
2012-01-21 10:57:44 +0100

    Plugin _ Load core base js class for thumbnail Plugin -  Added two events listeners for interaction between clichethumbnail TV and the attached album

--- core/components/cliche/elements/plugins/plugin.clichethumbnail.php
+++ core/components/cliche/elements/plugins/plugin.clichethumbnail.php
@@ -19,9 +19,11 @@ switch ($modx->event->name) {
         $modx->event->output($corePath.'elements/tv/properties/');
         break;
     case 'OnDocFormPrerender':	
+		$modx->controller->addCss($cliche->config['css_url'].'index.css');
 		$modx->controller->addCss($cliche->config['css_url'].'clichethumbnail.css');
 		
 		 /* assign cliche lang to JS */
+        $modx->controller->addLexiconTopic('cliche:mgr');
         $modx->controller->addLexiconTopic('cliche:clichethumbnail');
 		
 		$modx->controller->addHtml('<script type="text/javascript">
@@ -42,6 +44,10 @@ Ext.onReady(function() {
 		$modx->controller->addJavascript($cliche->config['assets_url'].'mgr/libs/plupload.js');
 		$modx->controller->addJavascript($cliche->config['assets_url'].'mgr/libs/plupload.html5.js');
 		$modx->controller->addJavascript($cliche->config['assets_url'].'mgr/libs/plupload.html4.js');
+		
+		/* Core base class */
+		$modx->controller->addJavascript($cliche->config['assets_url'].'mgr/core/album.js');
+		$modx->controller->addJavascript($cliche->config['assets_url'].'mgr/core/upload.js');
             
         /* TV panel classes */
 		$modx->controller->addJavascript($cliche->config['assets_url'].'mgr/thumbnail/tv/panel.js');
@@ -53,5 +59,66 @@ Ext.onReady(function() {
 		$modx->controller->addJavascript($cliche->config['assets_url'].'mgr/thumbnail/tv/cards/upload.js');
 		$modx->controller->addJavascript($cliche->config['assets_url'].'mgr/thumbnail/tv/cards/cropper.js');
         break;
+	case 'OnTemplateVarSave':
+		$type = $templateVar->get('type');
+		if($type == 'clichethumbnail'){
+			$name = $templateVar->get('name');
+			$id = $templateVar->get('id');
+			$properties = $templateVar->getProperties();
+			
+			/* Get the existing album */			
+			if($mode == 'upd' && isset($properties['clichealbum'])){					
+				$album = $modx->getObject('ClicheAlbums', $properties['clichealbum']);						
+			} 	
+			/* Prevents album duplication */
+			if($mode == 'upd' && !$album){									
+				$album = $modx->getObject('ClicheAlbums', array('description'=> 'Cliche Thumbnail TV - #' . $id));	
+				if($album){
+					$properties['clichealbum'] = $album->get('id');
+					$templateVar->setProperties(array('clichealbum' => $properties['clichealbum']), true);
+					$templateVar->save();
+				}
+			} 		
+			
+			/* Else create a new Cliche album */
+			if(!$album){
+				$album = $modx->newObject('ClicheAlbums');
+				$album->set('type', 'clichethumbnail');
+				$album->set('description', 'Cliche Thumbnail TV - #' . $id);
+			}
+			
+			/* We have an album */
+			if($album){					
+				$album->set('name', $name);
+				$album->save();				
+				if(!isset($properties['clichealbum'])){
+					$aid = $album->get('id');
+					$templateVar->setProperties(array('clichealbum' => $aid), true);
+					$templateVar->save();
+				}				
+			}
+			
+			unset($album);
+		}
+		break;
+	case 'OnTemplateVarBeforeRemove':
+		$type = $templateVar->get('type');
+		if($type == 'clichethumbnail'){
+			$name = $templateVar->get('name');
+			$properties = $templateVar->getProperties();
+			if(isset($properties['clichealbum'])){
+				$album = $modx->getObject('ClicheAlbums', $properties['clichealbum']);	
+				if($album){
+					$album->fromArray(array(
+						'name' => $name,
+						'type' => 'default',
+						'description' => 'This was a ClicheThumbnail dedicated album whose TV has been removed',
+					));
+					$album->save();
+				}
+			}
+		}
+		break;
+	default:break;
 }
 return;
